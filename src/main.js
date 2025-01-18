5ed13c64351a8aae9e9027e3f1a7c927c8262474
NN <noone@example.com>
2025-01-18 21:31:29 +0300

    Added better comments

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ function autocompleteCity(input, dataList, onChange) {
   input.addEventListener('keyup', (e) => {
     let enteredCity = e.target.value
 
+    /* Ignoring the keyup event if the user has entered less than two
+    characters or the entered characters are arrow keys, Enter or
+    Escape. Those keys, when used to navigate the <datalist>, will make
+    the <datalist> glitch if we don't return here. */
     if (
       enteredCity.length < 2 ||
       ['ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft', 'Enter', 'Escape'].includes(e.key))
@@ -12,6 +16,7 @@ function autocompleteCity(input, dataList, onChange) {
     }
 
     dataListOptions = []
+    // Ignoring everything after the first comma:
     enteredCity = enteredCity.trim().replace(/,.*/, '').toLowerCase()
     const options = []
 
@@ -19,6 +24,8 @@ function autocompleteCity(input, dataList, onChange) {
       if (city[0].toLowerCase().startsWith(enteredCity)) {
         let parts = [city[0]]
 
+        /* If the city is in a region, we'll add it. Not every city in
+        the data belongs to a region. */
         if (autocompleteCityNames.regions[city[2] + '.' + city[1]]) {
           parts.push(autocompleteCityNames.regions[city[2] + '.' + city[1]])
         }
@@ -26,17 +33,24 @@ function autocompleteCity(input, dataList, onChange) {
         parts.push(autocompleteCityNames.countries[city[2]])
 
         city.longName = parts.join(', ')
+        // Creating an <option> element for the <datalist>:
         options.push(new Option('', city.longName))
         dataListOptions.push(city)
       }
     }
 
+    /* Replacing the old <option> elements in the <datalist> with the
+    new ones: */
     dataList.replaceChildren(...options)
   })
 
+  /* Since there are no events that can be attached to a <datalist>,
+  the best way to detect when the user has selected a city is to use
+  the "change" event on the <input> element: */
   input.addEventListener('change', (e) => {
     for (let option of dataListOptions) {
       if (e.target.value === option.longName) {
+        // Running the user's callback function if supplied:
         if (onChange instanceof Function) {
           onChange({
             name: option[0],
