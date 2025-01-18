c9c74dfec26ec575da701cfb1802436df1a1f9b5
NN <noone@example.com>
2025-01-18 21:20:24 +0300

    Added comments

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,16 @@
+###################
+# Merges geoNames.js (from geonames2js) with a minified version of
+# main.js.
+# 
+# Requirements: uglifyjs.
+# Usage: sh build.sh
+###################
+
 OUTPUT_FILE=../autocompleteCity.js
 
+# Converts geoNames.js from an ES module to a script that can be easily
+# added to a webpage without using <script type="module">:
 sed -e 's/export default/const autocompleteCityNames=/' geoNames.js > $OUTPUT_FILE
 echo ';' >> $OUTPUT_FILE
+
 uglifyjs --compress --mangle -- main.js >> $OUTPUT_FILE
